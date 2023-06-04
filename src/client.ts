2650d5f08136b1e457028230b9c56f6db9addee3
Tony Yang <tonyyanga@gmail.com>
2023-06-04 12:48:06 -0700

    remove node

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,3 @@
 import Client from "./nextJsClient";
-import NodeClient from "./nodeClient";
 
-export { Client as NextJsClient, NodeClient };
-
-export default NodeClient
+export { Client as NextJsClient };
